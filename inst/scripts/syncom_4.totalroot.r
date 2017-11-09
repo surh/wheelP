5e7c7f5f9ce5b94da8ebfc4aa982e7e24f26ccd7
surh <surh@users.noreply.github.com>
2017-11-08 22:22:06 -0800

    Area uses new functions and plots SE

--- inst/scripts/syncom_4.totalroot.r
+++ inst/scripts/syncom_4.totalroot.r
@@ -1,17 +1,17 @@
 # (C) Copyright 2017 Sur Herrera Paredes
-# 
+#
 # This file is part of wheelP.
-# 
+#
 # wheelP is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # wheelP is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with wheelP.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -36,7 +36,6 @@ p1
 ggsave("totalroot_experiment.svg",p1,width = 5, height = 5)
 
 ## Fit one at a time
-
 dir <- "totalroot_images/"
 Res <- test_single_community_phenotype(Dat = Dat, dir = dir, var.name = "total.root",
                                        bacteria.col = "Bacteria", ref.level = "none",
@@ -52,11 +51,16 @@ dat$Block1 <- substring(dat$SynCom,1,2)
 dat$Block2 <- substring(dat$SynCom,3,4)
 dat$Block1 <- factor(dat$Block1 , levels = singlecoms)
 dat$Block2 <- factor(dat$Block2 , levels = rev(singlecoms))
+
+dat$Significant <- ""
+dat$Significant[ dat$p.value < 0.05 ] <- "X"
+
 head(dat)
 
 p1 <- ggplot(dat, aes(x = Block1, y = Block2)) +
   facet_grid(StartP ~ EndP) +
   geom_tile(aes(fill = Estimate), size = 1, col = "black") +
+  geom_text(aes(label = Significant), size = 8) +
   scale_fill_gradient2(low = c("#8e0152","#de77ae"),mid = "#f7f7f7",
                        high = c("#7fbc41","#276419"),
                        midpoint = 0,
@@ -72,42 +76,49 @@ ggsave("totalroot_images/heatmap_mix_estimate.svg",p1, width = 8, height = 7)
 rm(singlecoms,dat,p1,dir)
 
 ## Now we estimate the main effects
-singlecoms <- paste(rep(c("P","I","N"),each = 3),rep(1:3,times = 3),sep="")
+Res2 <- test_single_block_phenptype(Phen = Dat,
+                                    phenotype = 'total.root',
+                                    confounders = c("Experiment"),
+                                    use_abun = FALSE)
 
-Res2 <- NULL
-for(i in 1:2){
-  for(j in 1:2){
-    m1 <- block_effects(Dat = Dat, cond1 = i, cond2 = j,
-                        var.name = "total.root",
-                        keep.vars = c("Experiment"))
-    m1.sum <- summary(m1)
-
-    res <- data.frame(SynCom = singlecoms, StartP = levels(Dat$StartP)[i],
-                      EndP = levels(Dat$EndP)[j],
-                      Estimate = m1.sum$coefficients[ singlecoms, 1 ],
-                      SE = m1.sum$coefficients[ singlecoms, 2 ],
-                      t.value = m1.sum$coefficients[ singlecoms, 3 ],
-                      p.value = m1.sum$coefficients[ singlecoms, 4 ])
-    Res2 <- rbind(Res2,res)
-    # p1 <- coefplot(m1, intercept = FALSE,innerCI = 1, outerCI = 2,
-    #                coefficients = singlecoms,
-    #                lwdOuter = 0.5, lwdInner = 2.5, pointSize = 4,
-    #                color = "black", zeroColor = "red", zeroType = 1)
-    # p1 <- p1 + theme(axis.text.y = element_text(color = "black"),
-    #                  panel.border = element_rect(fill = NA, color = "black", size = 2),
-    #                  panel.background = element_rect(fill = "white")) +
-    #   ggtitle(paste(levels(Dat$StartP)[i],"=>",levels(Dat$EndP)[j]))
-    # p1
-    #
-    # filename <- paste(dir,"/coefplot_",i,j,".png",sep = "")
-    # ggsave(filename = filename, p1 , width = 3.5, height = 5)
-  }
-}
+# singlecoms <- paste(rep(c("P","I","N"),each = 3),rep(1:3,times = 3),sep="")
+#
+# Res2 <- NULL
+# for(i in 1:2){
+#   for(j in 1:2){
+#     m1 <- block_effects(Dat = Dat, cond1 = i, cond2 = j,
+#                         var.name = "total.root",
+#                         keep.vars = c("Experiment"))
+#     m1.sum <- summary(m1)
+#
+#     res <- data.frame(SynCom = singlecoms, StartP = levels(Dat$StartP)[i],
+#                       EndP = levels(Dat$EndP)[j],
+#                       Estimate = m1.sum$coefficients[ singlecoms, 1 ],
+#                       SE = m1.sum$coefficients[ singlecoms, 2 ],
+#                       t.value = m1.sum$coefficients[ singlecoms, 3 ],
+#                       p.value = m1.sum$coefficients[ singlecoms, 4 ])
+#     Res2 <- rbind(Res2,res)
+#     # p1 <- coefplot(m1, intercept = FALSE,innerCI = 1, outerCI = 2,
+#     #                coefficients = singlecoms,
+#     #                lwdOuter = 0.5, lwdInner = 2.5, pointSize = 4,
+#     #                color = "black", zeroColor = "red", zeroType = 1)
+#     # p1 <- p1 + theme(axis.text.y = element_text(color = "black"),
+#     #                  panel.border = element_rect(fill = NA, color = "black", size = 2),
+#     #                  panel.background = element_rect(fill = "white")) +
+#     #   ggtitle(paste(levels(Dat$StartP)[i],"=>",levels(Dat$EndP)[j]))
+#     # p1
+#     #
+#     # filename <- paste(dir,"/coefplot_",i,j,".png",sep = "")
+#     # ggsave(filename = filename, p1 , width = 3.5, height = 5)
+#   }
+# }
 write.table(Res2,"totalroot_block_test.txt",
             sep = "\t", quote = FALSE, row.names = FALSE)
 
 # compare predictions from main effects
 # with measured effects
+Pred <- predict_from_main(dat = Res, Res.main = Res2)
+head(Pred)
 
 # Format observed effects
 dat <- Res
