f3fbae6be117c1bdfc45e1f0f6e71725998ee202
surh <surh@users.noreply.github.com>
2017-11-09 17:12:25 -0800

    Alternatives for overal Pi_content distribution

--- inst/scripts/binP_1.allstrain_analysis.r
+++ inst/scripts/binP_1.allstrain_analysis.r
@@ -1,23 +1,23 @@
 # (C) Copyright 2017 Sur Herrera Paredes
-# 
+#
 # This file is part of wheelP.
-# 
+#
 # wheelP is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # wheelP is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with wheelP.  If not, see <http://www.gnu.org/licenses/>.
 
 library(wheelP)
 library(ggplot2)
-#setwd("~/rhizogenomics/experiments/2017/today8/")
+setwd("~/rhizogenomics/experiments/2017/today7/")
 #devtools::document("~/rhizogenomics/src/trunk/phosphate_code/wheelP/")
 
 # Read data
@@ -100,11 +100,65 @@ rm(Master)
 devtools::use_data(binP.all, pkg = "~/rhizogenomics/src/trunk/phosphate_code/wheelP/",
                    overwrite = TRUE)
 
+###########
+data(binP.all)
+
 # Plot all strains together
 p1 <- plot_binary(binP.all)
 p1 <- p1 + geom_vline(xintercept = 10)
 p1
 ggsave("twoway_allstrains.svg",p1 + geom_vline(xintercept = 10),width = 10, height = 8)
+dat <- p1$data
+
+dat$Condition <- NULL
+dat$Condition[ dat$StartP == "-Pi,0.5%Suc" & dat$EndP == "30uM,0%Suc"] <- '-P 30uM'
+dat$Condition[ dat$StartP == "-Pi,0.5%Suc" & dat$EndP == "100uM,0%Suc"] <- '-P 100uM'
+dat$Condition[ dat$StartP == "+Pi,0.5%Suc" & dat$EndP == "30uM,0%Suc"] <- '+P 30uM'
+dat$Condition[ dat$StartP == "+Pi,0.5%Suc" & dat$EndP == "100uM,0%Suc"] <- '+P 100uM'
+dat$Condition <- factor(dat$Condition, levels = c('-P 100uM','-P 30uM','+P 100uM','+P 30uM'))
+dat$Bacteria <- factor(dat$Bacteria, levels = c('+Bacteria','No Bacteria'))
+head(dat)
+p1 <- ggplot(dat,aes(x = Condition, y = Pi_content, color = Bacteria)) +
+  geom_boxplot(outlier.colour = NA) +
+  geom_point(position = position_jitterdodge(), size = 0.7, alpha = 0.3) +
+  geom_hline(yintercept = 10, color = 'black') +
+  scale_y_log10(breaks = c(1, 2, 5, 10, 20, 50)) +
+  scale_x_discrete(labels = c(expression(paste('-P 100',mu,M)),
+                              expression(paste('-P 30',mu,M)),
+                              expression(paste('+P 100',mu,M)),
+                              expression(paste('+P 30',mu,M)))) +
+  theme(panel.background = element_rect(fill = NA),
+        panel.border = element_rect(color = "black",fill = NA, size = 3),
+        axis.text.y = element_text(color = 'black', size = 20),
+        axis.title.y = element_text(face = 'bold', color = 'black', size = 24),
+        axis.title.x = element_text(face = "bold", color = "black", size = 24),
+        axis.text.x = element_text(color = "black", size = 20),
+        strip.text = element_text(color = "black", size = 32, face = "bold"),
+        legend.text = element_text(size = 20),
+        legend.title = element_text(size = 20))
+p1
+ggsave('overall_pi_boxplot.svg', p1, width = 10, height = 8)
+
+p1 <- ggplot(dat,aes(x = Condition, y = Pi_content, color = Bacteria, fill = Bacteria)) +
+  geom_hline(yintercept = 10, color = 'grey', size = 3) +
+  geom_violin(draw_quantiles = c(0.25,0.5,0.75), alpha = 0.4) +
+  #geom_point(position = position_jitterdodge(), size = 0.7, alpha = 0.3) +
+  scale_y_log10(breaks = c(1, 2, 5, 10, 20, 50)) +
+  scale_x_discrete(labels = c(expression(paste('-P 100',mu,M)),
+                              expression(paste('-P 30',mu,M)),
+                              expression(paste('+P 100',mu,M)),
+                              expression(paste('+P 30',mu,M)))) +
+  theme(panel.background = element_rect(fill = NA),
+        panel.border = element_rect(color = "black",fill = NA, size = 3),
+        axis.text.y = element_text(color = 'black', size = 20),
+        axis.title.y = element_text(face = 'bold', color = 'black', size = 24),
+        axis.title.x = element_text(face = "bold", color = "black", size = 24),
+        axis.text.x = element_text(color = "black", size = 20),
+        strip.text = element_text(color = "black", size = 32, face = "bold"),
+        legend.text = element_text(size = 20),
+        legend.title = element_text(size = 20))
+p1
+ggsave('overall_pi_violin.svg', p1, width = 10, height = 8)
 
 # Test overall bacteril effect
 m1 <- aov(log(Pi_content) ~ Bacteria * StartP * EndP + Experiment ,data = binP.all)
