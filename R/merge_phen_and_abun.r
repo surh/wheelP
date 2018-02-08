2c0cef201777dade4b8ec52b9130976294c87324
surh <sur00mx@gmail.com>
2018-02-07 17:09:43 -0800

    documenting merge_phen_and_abun function

--- R/merge_phen_and_abun.r
+++ R/merge_phen_and_abun.r
@@ -1,4 +1,25 @@
 #' Merge phenotype data with relative abundances of blocks
+#' 
+#' Merge two data frames containing plant phenotypic data
+#' and bacterial relative abundances
+#' 
+#' The function takes a list of variables that when combined must
+#' uniqueley identify each row of both data frames. Those variable smust exist
+#' in both data frames. 
+#' 
+#' It then constructs an index that it uses to match the columns. Abundance
+#' data can be missing for some phenotypic observations, in which case
+#' it will be replaced by zero.
+#' 
+#' @param Phen A data frame with phenotypic information
+#' @param abun A data frame with abundance information
+#' @param varnames Vector of charachter strings indicating which
+#' variables to use to combine datasets. Rows where the exact same
+#' combination of values for all variables occur are considered identical.
+#' 
+#' @author Sur Herrera Paredes
+#' 
+#' @keywords utils syncom colonization
 #'
 #' @export
 merge_phen_and_abun <- function(Phen, abun, columns,
