c194c42b987a77dd70ed5445d6b8ea6b3a93741b
surh <sur00mx@gmail.com>
2018-02-09 16:41:38 -0800

    Documenting obtain_block_abundances function

--- R/obtain_block_abundances.r
+++ R/obtain_block_abundances.r
@@ -20,8 +20,19 @@
 #' Takes a Dataset object, pools samples and taxa according to
 #' specified variables, and returns  adata frame that contains
 #' the average abundance by taxa group per group of samples
+#' 
+#' @param Dat a dataset object
+#' @param varnames Vector of character strings that name the variables
+#' to be used to pool samples. Must correspond to variables in
+#' the Map attribute of Dat.
+#' @param taxa.group Character string indicating the variable for
+#' collapsing taxa.
+#' @param sep Separator character for creating composite variables.
+#' @param taxa2rm Taxa IDs to remove.
 #'
 #' @author Sur Herrera Paredes
+#' 
+#' @keywords colonization syncom
 #'
 #' @export
 obtain_block_abundances <- function(Dat, varnames = c("Bacteria","Replicate","Experiment","Pre.Pi", "Pos.Pi"),
