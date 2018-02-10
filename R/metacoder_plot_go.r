e41775e2bb7837453517f4c14e3e4bc930328fbd
surh <sur00mx@gmail.com>
2018-02-09 16:34:15 -0800

    Documenting metacoder_plot_go function

--- R/metacoder_plot_go.r
+++ R/metacoder_plot_go.r
@@ -16,8 +16,40 @@
 # along with wheelP.  If not, see <http://www.gnu.org/licenses/>.
 
 #' Plot GO from edgeR
+#' 
+#' Creates a metacoder network plot from gene ontologies. Figures from this function
+#' where not included in final manuscript.
+#' 
+#' The script takes a list of differentially expressed genes and their GO annotations
+#' and creates a tree representing the gene ontologies present and color codes it by
+#' the overal log(fold-change) im expression/
+#' 
+#' @param dat A data frame containing the results of differential expression analysis on
+#' edgeR. The data frame is the result of performing hypothesis testing, for example
+#' with the likelihood ratio test (i.e. function \code{\link{glmLRT}}).
+#' @param output_folder Charachter string indicating the directory to store the
+#' plots.
+#' @param output_format Format of the image to save. It must be allowed
+#' by the \code{\link{heat_tree}} function.
+#' @param min_fdr False discovery rate threshold to filter genes. Only genes
+#' with a smaller fdr and their annotations will be included.
+#' @param type This must be an object that specifies the parents of each
+#' annotation terms. Default is object GOBPPARENTS fromthe GO.db bioconductor
+#' package, but any other simular object can be used.
+#' @param prefix Character string. Prefix for file names.
+#' @param n.supertaxa Number of levels to extend the tree.
+#' @param num.changed Minimum number of genes in an annotation category
+#' fir that annotation to be included.
 #'
-#' @author from metacoder paper
+#' @author Code from metacoder paper with small adaptations by Sur Herrera Paredes
+#' 
+#' @references Foster ZSL, Sharpton TJ, Grünwald NJ (2017) Metacoder:
+#' An R package for visualization and manipulation of community taxonomic diversity data.
+#' PLoS Comput Biol 13(2): e1005404. https://doi.org/10.1371/journal.pcbi.1005404
+#' 
+#' @keywords rna plots
+#' 
+#' @seealso \code{\link{parse_tax_and_plot}} \code{\link{heat_tree}}
 #'
 #' @export
 metacoder_plot_go <- function(dat, output_folder = "./", output_format = "svg",
